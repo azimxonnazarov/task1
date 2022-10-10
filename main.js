4fd22bfe768bdbeac0195f5618c601d6d7287271
Azim <777@ru>
2022-10-10 21:21:48 +0500

    last

---
+++ main.js
@@ -0,0 +1,22 @@
+
+var str = prompt('Введите своё имя');
+console.log('Меня зовут ' + str);
+
+var age = +prompt('Введите свой возраст');
+console.log('Мне ' + age);
+
+
+var task = +prompt('10 + 4');
+console.log('Пример 1: ' + '10 + 4 = ' + (10 + 4) + ' Ваш ответ ' + task);
+
+var test = +prompt('10 - 4');
+console.log('Пример 2: ' + '10 - 4 = ' + (10 - 4) + ' Ваш ответ ' + test);
+
+var answer = +prompt('10 * 4');
+console.log('Пример 3: ' + '10 * 4 = ' + 10 * 4 + ' Ваш ответ ' + answer);
+
+var info = +prompt('10 / 4');
+console.log('Пример 4: ' + '10 / 4 = ' + 10 / 4 + ' Ваш ответ ' + info);
+
+var percent = +prompt('10 % 4');
+console.log('Пример 5: ' + '10 % 4 = ' + (10 % 4) + ' Ваш ответ ' + percent);
